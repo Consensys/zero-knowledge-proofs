aed7d7dc6216c9d585f36896a4d0ea1c58abea8b
Peter Munnings <petermunnings@gmail.com>
2017-09-01 17:23:49 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -2,47 +2,19 @@ var prompt = require('prompt');
 
 const {exec} = require( 'child_process' )
 
-function handleGenerateKeyPair(cb){
-
-  exec('./generateKeyPair', (error, stdout, stderr) => {
+function handleExecuteProgram(programName, msgStart, msgEnd, msgError, cb){
+  console.log(msgStart)
+  exec(programName, (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`)
     if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-    cb()
-  });
-
-}
-
-function handleGenerateProof(cb){
-  console.log('Loading Proving Key from file... (this takes a few seconds)')
-  exec('./generateProof', (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    if (error) {
-      console.error(`exec error: ${error}`);
-      console.log('The proof generation failed\n\n');
+      console.error(`exec error: ${error}`)
+      console.log(msgError)
     }
     if(stderr){
-      console.log(`stderr: ${stderr}`);
-      console.log('The proof generation failed\n\n');
+      console.log(`stderr: ${stderr}`)
+      console.log(msgError)
     }
-    cb()
-  });
-}
-
-function handleVerifyProof(cb){
-  exec('./verifyProof', (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    if (error) {
-      console.error(`exec error: ${error}`);
-      console.log('The proof verification failed\n\n');
-    }
-    if(stderr){
-      console.log(`stderr: ${stderr}`);
-      console.log('The proof verification failed\n\n');
-    }
+    console.log(msgEnd)
     cb()
   });
 }
@@ -51,15 +23,15 @@ function handleInput(){
   console.log('Please select an option:\n1) Create a new key pair\n2) Generate a proof\n3) Verify a proof\n4) Quit')
   prompt.get(['option'], function(err, answer){
     if(answer.option == 1){
-      handleGenerateKeyPair(function(){
+      handleExecuteProgram('./generateKeyPair', 'Generating key pair...', 'The key pair has been generated and the keys written to files (provingKey and verificationKey)', 'The key pair failed\n\n', function(){
         handleInput()
       })
     } else if (answer.option == 2){
-      handleGenerateProof(function(){
+      handleExecuteProgram('./generateProof', 'Loading Proving Key from file... (this takes a few seconds)', '', 'The proof generation failed\n\n', function(){
         handleInput()
       })
     } else if(answer.option == 3){
-      handleVerifyProof(function(){
+      handleExecuteProgram('./verifyProof', '', '', 'The proof verification failed\n\n', function(){
         handleInput()
       })
     } else {
