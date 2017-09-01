c1d861486d01dc613685c6d914a23d896d9a5b97
Peter Munnings <petermunnings@gmail.com>
2017-09-01 11:14:02 +0200

    Added a command line for generating and validating proofs

--- src/verifyProof.cpp
+++ src/verifyProof.cpp
@@ -23,8 +23,6 @@ int main(int argc, char *argv[])
   }
   verificationKeyFromFile >> verificationKey_in;
 
-  cout << "verificationKey_in" << verificationKey_in << endl;
-  
   // Read proof in from file
   //libsnark::r1cs_ppzksnark_proof<libff::alt_bn128_pp> proof_in;
   boost::optional<libsnark::r1cs_ppzksnark_proof<libff::alt_bn128_pp>> proof_in;
@@ -35,8 +33,13 @@ int main(int argc, char *argv[])
   if (proofFileIn) {
      proofFromFile << proofFileIn.rdbuf();
      proofFileIn.close();
+  } else {
+    cout << "Failed to read from proof file" << endl;
+    return 1;
   }
+
   proofFromFile >> proof_in;
+  
   
   // Hashes to validate against
   std::vector<bool> h1_bv(256);
@@ -46,15 +49,18 @@ int main(int argc, char *argv[])
   h2_bv = int_list_to_bits({182, 169, 95, 91, 248, 154, 156, 163, 104, 18, 251, 174, 68, 251, 237, 249, 215, 166, 135, 222, 50, 133, 48, 197, 197, 205, 182, 20, 56, 166, 108, 66}, 8);
   h3_bv = int_list_to_bits({101, 119, 48, 144, 165, 169, 249, 100, 249, 74, 13, 126, 39, 34, 64, 47, 238, 173, 29, 72, 31, 203, 7, 100, 179, 20, 220, 66, 172, 97, 252, 223}, 8);
  
+  cout << "proof read ... starting verification" << endl;
   // Verify the proof
   bool isVerified = verify_proof(verificationKey_in, *proof_in, h1_bv, h2_bv, h3_bv);
 
   if(isVerified){
     cout << "Proof was verified!!" << endl;
+    return 0;
   } else {
     cout << "Proof was not verified!!" << endl;
+    return 1;
   }
 
-  return 0;
+  return 1;
 }
 
